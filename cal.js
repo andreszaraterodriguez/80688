efe9d011fcd581a6341ccbcc9958692b7eb19134
andreszaraterodriguez <correo@gmail.com>
2021-09-14 13:15:13 -0500

    calculadora diseño

--- cal.js
+++ cal.js
@@ -1,8 +1,9 @@
-pantalla=document.getElementById("textoPantalla"); 
-x=0; 
-function numero(xx){
-   if (x=="0" || xi==1 ) {
-   pantalla.innerHTML="0";
-   x=xx;
-   }
+var numpantalla="0";
+
+function ingresarnum(x)
+{
+ document.calculadora.txt.value=x;
+ numpantalla=x;
+	
 }
+
